f41bbc685bda8b503fd6b27975cf642c901597cd
Marko Radulovic <markoradulovic@twognation.com>
2021-10-04 15:11:50 +0200

    characters list with pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { Apollo, gql } from 'apollo-angular';
 
 @Component({
@@ -8,6 +9,8 @@ import { Apollo, gql } from 'apollo-angular';
 })
 export class AppComponent implements OnInit {
   characters: any[] = [];
+  length = 600;
+  pageSize = 20;
 
   constructor(private apollo: Apollo) {}
 
@@ -16,7 +19,25 @@ export class AppComponent implements OnInit {
       .watchQuery({
         query: gql`
           {
-            characters(page: 1, filter: { species: "Alien" }) {
+            characters(page: 1) {
+              results {
+                name
+              }
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.characters = result?.data?.characters?.results;
+      });
+  }
+
+  change(event: PageEvent) {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            characters(page: ${event.pageIndex + 1}) {
               results {
                 name
               }
